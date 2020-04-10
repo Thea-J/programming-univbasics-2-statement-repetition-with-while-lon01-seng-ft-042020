8670df3754b6315dee02f903a82eabad4317a068
Dorothy John <dorothyy.john@gmail.com>
2020-04-10 14:09:51 +0000

    Done.

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,11 @@
-# Write your code here
+#Countdown from 10 to 10
+# Display each value
+#Output "Happy new year!" after 1 is displayed
+i=10
+while i>0 do
+  puts "#{i}"
+    if i==1 
+    puts"Happy New Year!"
+  end
+  i-=1
+end
